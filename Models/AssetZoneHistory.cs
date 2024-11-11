59aae9f87dc5517f5f5363496133fc9d26c31f37
tpapic21 <tpapic21@foi.hr>
2024-11-11 21:40:42 +0100

    Add db models

---
+++ Models/AssetZoneHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndoorLocalization_API.Models;
+
+public partial class AssetZoneHistory
+{
+    public int Id { get; set; }
+
+    public int? AssetId { get; set; }
+
+    public int? ZoneId { get; set; }
+
+    public DateTime? EnterDateTime { get; set; }
+
+    public DateTime? ExitDateTime { get; set; }
+
+    public string? RetentionTime { get; set; }
+
+    public virtual Asset? Asset { get; set; }
+
+    public virtual Zone? Zone { get; set; }
+}
---
+++ Models/FloorMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndoorLocalization_API.Models;
+
+public partial class FloorMap
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Image { get; set; }
+
+    public virtual ICollection<AssetPositionHistory> AssetPositionHistories { get; set; } = new List<AssetPositionHistory>();
+
+    public virtual ICollection<Asset> Assets { get; set; } = new List<Asset>();
+}
