d8859bb83dc47feb64c2e8081a6cc2ac2173d063
tpapic21 <tpapic21@foi.hr>
2024-11-26 18:04:43 +0100

    Add: Add new db models

---
+++ Models/Zone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IndoorLocalization_API.Models;
+
+public partial class Zone
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Points { get; set; }
+
+    public virtual ICollection<AssetZoneHistory> AssetZoneHistories { get; set; } = new List<AssetZoneHistory>();
+}
