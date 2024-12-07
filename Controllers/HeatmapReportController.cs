d9408ea4c75ab053b776a2fa80830e6276186e5d
Toni Papić <tpapic21@foi.hr>
2024-12-07 18:57:51 +0100

    Add GET method for getting asset position history by assetID

---
+++ Controllers/HeatmapReportController.cs
@@ -0,0 +1,27 @@
+﻿using IndoorLocalization_API.Database;
+using IndoorLocalization_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndoorLocalization_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeatmapReportController : APIDatabaseContext<AssetPositionHistory>
+    {
+        public HeatmapReportController(IndoorLocalizationContext context) : base(context){}
+
+        [HttpGet]
+        [Route("GetAssetPositionHistory/{assetId}")]
+        public async Task<List<AssetPositionHistory>> GetAssetPositionHistoriesAsync(int assetId)
+        {
+            
+            var assetPositionHistory = await _context.AssetPositionHistories.Where(p=>p.AssetId==assetId).ToListAsync();
+            if (assetPositionHistory == null)
+            { 
+                return new List<AssetPositionHistory>();
+            }
+            return assetPositionHistory;
+        }
+    }
+}
