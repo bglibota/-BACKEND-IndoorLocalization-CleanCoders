98e5b58b42aaa81fae537797569e885b58a0af4a
Toni Papić <tpapic21@foi.hr>
2024-12-07 20:07:56 +0100

    Add more GET methods for AssetPositionHistory

--- Controllers/HeatmapReportController.cs
+++ Controllers/HeatmapReportController.cs
@@ -9,19 +9,55 @@ namespace IndoorLocalization_API.Controllers
     [Route("[controller]")]
     public class HeatmapReportController : APIDatabaseContext<AssetPositionHistory>
     {
-        public HeatmapReportController(IndoorLocalizationContext context) : base(context){}
+        public HeatmapReportController(IndoorLocalizationContext context) : base(context) { }
 
         [HttpGet]
         [Route("GetAssetPositionHistory/{assetId}")]
-        public async Task<List<AssetPositionHistory>> GetAssetPositionHistoriesAsync(int assetId)
+        public async Task<ActionResult<List<AssetPositionHistory>>> GetAssetPositionHistoriesAsync(int assetId)
         {
-            
-            var assetPositionHistory = await _context.AssetPositionHistories.Where(p=>p.AssetId==assetId).ToListAsync();
-            if (assetPositionHistory == null)
-            { 
-                return new List<AssetPositionHistory>();
-            }
-            return assetPositionHistory;
+            var assetPositionHistory = await _context.AssetPositionHistories
+                .Where(p => p.AssetId == assetId)
+                .ToListAsync();
+
+            return assetPositionHistory ?? new List<AssetPositionHistory>();
+        }
+
+        [HttpGet]
+        [Route("GetAssetPositionHistoryByDate/{assetId}/{date}")]
+        public async Task<ActionResult<List<AssetPositionHistory>>> GetAssetPositionHistoriesByDateAsync(int assetId, DateTime date)
+        {
+            var assetPositionHistory = await _context.AssetPositionHistories
+                .Where(p => p.AssetId == assetId && p.DateTime.Value.Date == date.Date)
+                .ToListAsync();
+
+            return assetPositionHistory ?? new List<AssetPositionHistory>();
+        }
+
+        [HttpGet]
+        [Route("GetAssetPositionHistoryByDateAndTimeRange/{assetId}/{date}/{startTime}/{endTime}")]
+        public async Task<ActionResult<List<AssetPositionHistory>>> GetAssetPositionHistoriesByDateRangeAsync(int assetId, DateTime date, TimeSpan startTime, TimeSpan endTime)
+        {
+            var assetPositionHistory = await _context.AssetPositionHistories
+                .Where(p => p.AssetId == assetId
+                            && p.DateTime.Value.Date == date.Date
+                            && p.DateTime.Value.TimeOfDay >= startTime
+                            && p.DateTime.Value.TimeOfDay <= endTime)
+                .ToListAsync();
+
+            return assetPositionHistory ?? new List<AssetPositionHistory>();
+        }
+
+        [HttpGet]
+        [Route("GetAssetPositionHistoryByTimeRange/{assetId}/{startTime}/{endTime}")]
+        public async Task<ActionResult<List<AssetPositionHistory>>> GetAssetPositionHistoriesByTimeRangeAsync(int assetId, TimeSpan startTime, TimeSpan endTime)
+        {
+            var assetPositionHistory = await _context.AssetPositionHistories
+                .Where(p => p.AssetId == assetId
+                            && p.DateTime.Value.TimeOfDay >= startTime
+                            && p.DateTime.Value.TimeOfDay <= endTime)
+                .ToListAsync();
+
+            return assetPositionHistory ?? new List<AssetPositionHistory>();
         }
     }
 }
