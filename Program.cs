14e579883cbc89c7c1533ceed8e9c0f97bb63cf1
Toni Papić <tpapic21@foi.hr>
2024-12-10 12:05:02 +0100

    Fix: Change connection string

--- Program.cs
+++ Program.cs
@@ -1,15 +1,22 @@
 using IndoorLocalization_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration; // Add this using directive
+using Microsoft.Extensions.DependencyInjection; // Add this using directive
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<IndoorLocalizationContext>();
+//builder.Services.AddDbContext<IndoorLocalizationContext>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddUserSecrets<Program>();
+builder.Services.AddDbContext<IndoorLocalizationContext>(options =>
+      options.UseNpgsql(Environment.GetEnvironmentVariable("DefaultConnection")));
+
 
 var app = builder.Build();
 
